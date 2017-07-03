b09db7bab8158a0f0e58d50f86112411062a541a
cperceful <cpercef1@gmail.com>
2017-07-03 17:42:13 -0500

    Completed class 3 studio

--- HelloMVC/Controllers/HelloController.cs
+++ HelloMVC/Controllers/HelloController.cs
@@ -6,23 +6,52 @@ namespace HelloMVC.Controllers
 {
     public class HelloController : Controller
     {
+        static int count = 0;
+
         // GET: /<controller>/
         [HttpGet] //specifies this method responds to get requests
         public IActionResult Index() 
         {
             string html = "<form method='post'>" +
                 "<input type 'text' name='name' />" +
+                "<select name='language'>" +
+                "<option value='english'>English</option>" +
+                "<option value='russian'>Russian</option>" +
+                "<option value='romulan'>Romulan</option>" +
+                "<option value='german'>German</option>" +
+                "<option value='spanish'>Spanish</option>" +
+                "</select>" +
                 "<input type ='submit' value='Greet me!' />" +
                 "</form>";
 
-           return Redirect("/hello/goodbye"); //redirect
+           return Content(html, "text/html"); //redirect
         }
 
         [Route("/hello")] //maps to /hello
         [HttpPost] //responds to post requests at /hello
-        public IActionResult Display(string name = "world")
+        public IActionResult Display(string name = "friend", string language = "english")
         {
-            return Content(string.Format("<h1>What up, {0}</h1>", name), "text/html");
+            count++;
+            string greeting = "Hello";
+            
+            if (language == "english")
+            {
+                greeting = "Hello";
+            } else if (language == "russian")
+            {
+                greeting = "Privyet";
+            } else if (language == "romulan")
+            {
+                greeting = "Jolan Tru";
+            } else if (language == "german")
+            {
+                greeting = "Guten Tag";
+            } else if (language == "spanish")
+            {
+                greeting = "Hola";
+            }
+
+            return Content(string.Format("<h1>{0}, {1}!</h1> <h2>You've been greeted {2} times!</h2>", greeting, name, count), "text/html");
         }
 
         //Handle requests to /hello/NAME (URL segment)
